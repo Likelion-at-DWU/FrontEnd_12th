7e708b17c12d717a7e8b5b7b70ad8315f98f41f9
minyeong3201 <164001002+minyeong3201@users.noreply.github.com>
2024-05-09 19:05:38 +0900

    [4]_조민영_과제

---
+++ [4]_조민영_과제/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import TimerComponent from './TimerComponent.jsx';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>타이머</h1>
+      <TimerComponent />
+    </div>
+  );
+}
+
+export default App;
