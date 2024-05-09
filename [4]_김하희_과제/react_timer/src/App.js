fa0ffc70562bf6905d03a12802d5835240b095be
hahee <hahee_hi@naver.com>
2024-05-09 17:30:43 +0900

    [4]_김하희_과제_재재도전

---
+++ [4]_김하희_과제/react_timer/src/App.js
@@ -0,0 +1,11 @@
+import TimerComponent from "./TimerComponent";
+import useTimer from "./useTimer";
+
+function App() {
+  return (
+    <div>
+      <TimerComponent />
+    </div>
+  );
+}
+export default App;
