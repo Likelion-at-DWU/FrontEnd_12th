4b42a66dc9c560317f0a58f03ffbb683cae3ec05
sun <giomong2346@naver.com>
2024-05-31 14:25:39 +0900

    [6]_선지오_과제

---
+++ [6]_선지오_과제/letterbox_js/js/send.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const titleInput = document.querySelector(".titleInput");
+    const contentInput = document.querySelector(".contentInput");
+    const sendBtn = document.querySelector(".sendBtn");
+  
+    sendBtn.addEventListener("click", () => {
+      const title = titleInput.value;
+      const content = contentInput.value;
+  
+      console.log("제목:", title);
+      console.log("내용:", content);
+    });
+  });
